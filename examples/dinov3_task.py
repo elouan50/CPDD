03d3c87fecc2daa2a28b98d7b8f917540bc0b2b3
elouan50 <elouan.colybes@gmail.com>
2025-12-08 13:56:09 +0100

    feat dinov3 example

---
+++ examples/dinov3_task.py
@@ -0,0 +1,23 @@
+import lightly_train
+import matplotlib.pyplot as plt
+from torchvision.io import read_image
+from torchvision.utils import draw_bounding_boxes
+
+
+if __name__ == "__main__":
+    # Use the DINOv3 provided by LightlyTrain
+    model = lightly_train.load_model("dinov3/convnext-small-ltdetr-coco")
+
+    prediction = model.predict("examples/image.jpg")
+
+    image = read_image("examples/image.jpg")
+    image_with_boxes = draw_bounding_boxes(
+        image,
+        boxes=prediction["bboxes"],
+        labels=[model.classes[label.item()] for label in prediction["labels"]],
+    )
+    plt.imshow(image_with_boxes.permute(1, 2, 0))
+    plt.show()
+    # print(prediction)
+    # print(model.classes)
+        
